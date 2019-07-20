708ae2ef9c361e9bc79c765d07a9b4384a01623e
Eliza Munroe <era3munroe@gmail.com>
2019-07-20 10:27:14 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,20 @@
 def unsafe?(speed)
+  if speed < 40 
+    return true
+  elsif speed > 60
+    return true
+  else
+    return false
+end
+
+return speed
 
 end
 
 
 
 def not_safe?(speed)
-	
+	speed == (>60, 40...60) ? true : false
 end
 	
 
